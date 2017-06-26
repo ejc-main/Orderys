660e3a9256a4c6616ddd380a345cb84327334e5f
mrworth <markworth89@gmail.com>
2017-06-26 18:42:50 -0400

    Station Dao CRUD

--- Orderys/src/main/java/com/revature/orderys/dao/StationDaoImpl.java
+++ Orderys/src/main/java/com/revature/orderys/dao/StationDaoImpl.java
@@ -1,14 +1,17 @@
 package com.revature.orderys.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.orderys.bean.Station;
+import com.revature.orderys.bean.User;
 import com.revature.orderys.util.EasyLogger;
 
 @Transactional
@@ -23,75 +26,47 @@ public class StationDaoImpl {
 	
 	@SuppressWarnings("unchecked")
 	public List<Station> getAllStations() {
-		List<Station> stations = null;
+		List<Station> stations = new ArrayList<Station>();
 		Session session = sessionFactory.getCurrentSession();
 		try {
-			Transaction tx = session.beginTransaction();
 			stations = (List<Station>) session.createCriteria(Station.class).list();
-			tx.commit();
 		} catch (HibernateException ex) {
 			logger.catching(ex);
-		} finally {
-			session.close();
 		}
 		return stations;
 	}
 	
+	@Transactional(readOnly=false,propagation=Propagation.REQUIRED)
 	public void createStation(Station s) {
-		Session session = sessionFactory.getCurrentSession();
 		try {
-			Transaction tx = session.beginTransaction();
+			Session session = sessionFactory.getCurrentSession();
 			session.save(s);
-			tx.commit();
 		} catch (HibernateException ex) {
 			logger.catching(ex);
-		} finally {
-			session.close();
 		}
-		
 	}
 	
-	public Station getStationById(int id) {
-		Station s = null;
+	public Station getStationById(long id) {
 		Session session = sessionFactory.getCurrentSession();
-		try {
-			Transaction tx = session.beginTransaction();
-			
-			tx.commit();
-		} catch (HibernateException ex) {
-			logger.catching(ex);
-		} finally {
-			session.close();
-		}
-		return s;
+		return (Station) session.get(Station.class,id);
 	}
 	
 	public void updateStation(Station s) {
-		Session session = sessionFactory.getCurrentSession();
 		try {
-			Transaction tx = session.beginTransaction();
-			
-			tx.commit();
-		} catch (HibernateException ex) {
+			Session session = sessionFactory.getCurrentSession();
+			session.saveOrUpdate(s);
+		}catch (HibernateException ex) {
 			logger.catching(ex);
-		} finally {
-			session.close();
 		}
-		
 	}
 	
-	public void deleteStation(int id) {
-		Session session = sessionFactory.getCurrentSession();
+	public void deleteStation(Station s) {
 		try {
-			Transaction tx = session.beginTransaction();
-			
-			tx.commit();
+			Session session = sessionFactory.getCurrentSession();
+			session.delete(s);
 		} catch (HibernateException ex) {
 			logger.catching(ex);
-		} finally {
-			session.close();
 		}
-		
 	}
 
 }
