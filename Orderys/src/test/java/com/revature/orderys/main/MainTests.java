53960bed3fc97f6e4f300350b5ae61443837c42b
Eric <ericjchristie@gmail.com>
2017-06-27 18:09:36 -0400

    adding first test

---
+++ Orderys/src/test/java/com/revature/orderys/main/MainTests.java
@@ -0,0 +1,37 @@
+package com.revature.orderys.main;
+
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.orderys.bean.User;
+import com.revature.orderys.dao.UserDao;
+
+@Transactional
+public class MainTests {
+
+	@Test
+	public void test() {
+		AbstractApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		//user test block
+		UserDao dao = (UserDao) ac.getBean("userDao");
+		User u = new User("wowot@gmail.com","wwwt","atot","afkaowk",User.Role.CUSTOMER);
+		dao.createUser(u);
+		
+//		ArrayList<User> users=(ArrayList<User>) dao.getAllUsers();
+//		for(User user:users){
+//			System.out.println(user.getEmail());
+//		}
+//		User u1 = dao.getUserById(5);
+//		System.out.println(u1.getFirstName()+"   "+u1.getLastName());
+//		dao.deleteUser(u);
+		
+		//station test block
+//		StationDao dao = (StationDao) ac.getBean("stationDao");
+//		Station s = new Station();
+		
+		ac.close();
+	}
+
+}
