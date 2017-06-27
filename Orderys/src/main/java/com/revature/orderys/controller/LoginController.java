76795dc21fd51648525c9d4953edce71f1298bca
Eric <ericjchristie@gmail.com>
2017-06-26 22:22:46 -0400

    starting work on controllers

---
+++ Orderys/src/main/java/com/revature/orderys/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.revature.orderys.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.revature.orderys.service.UserService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private UserService userService;
+}
