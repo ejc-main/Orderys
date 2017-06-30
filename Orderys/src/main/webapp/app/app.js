463b91c0b93c4958da0e58510666a454c15c625a
Eric <ericjchristie@gmail.com>
2017-06-30 16:40:10 -0400

    did some stuffs

--- Orderys/src/main/webapp/app/app.js
+++ Orderys/src/main/webapp/app/app.js
@@ -1,1 +1,92 @@
 var orderysApp = angular.module('orderysApp',[]);
+
+var demoData = {
+  "exampleCustomer": {
+    "id": 1,
+    "email": "customer@email.com",
+    "firstname": "Custo",
+    "lastname": "One",
+    "role": "CUSTOMER",
+    "orders": [],
+    "ratings": []
+  },
+  "exampleEmployee": {
+    "id": 2,
+    "email": "employee@email.com",
+    "firstname": "Emplo",
+    "lastname": "Du",
+    "role": "EMPLOYEE",
+    "orders": [],
+    "ratings": [],
+    "stations": []
+  },
+  "exampleManager": {
+    "id": 3,
+    "email": "manager@email.com",
+    "firstname": "Tre",
+    "lastname": "Manag",
+    "role": "MANAGER",
+    "orders": [],
+    "ratings": [],
+    "stations": [],
+    "business": {}
+  },
+  "exampleBusiness": {
+    "id": 1,
+    "name": "",
+    "manager": {},
+    "streetAddress1": "",
+    "streetAddress2": "",
+    "city": "",
+    "state": "",
+    "country": "",
+    "zip": "",
+    "stations": []
+  },
+  "exampleStation": {
+    "id": 1,
+    "business": {},
+    "name": "",
+    "employees": []
+  },
+  "exampleProducts": [{
+    "id": 1,
+    "station": {},
+    "name": "",
+    "price": 12.00,
+    "description": "",
+    "intendedCompletionTime": "???",
+    "imageUrl": "",
+    "ratings": []
+  }],
+  "exampleOrder": {
+    "id": 1,
+    "customer": {},
+    "paymentMethod": "?? paypal ??",
+    "items": []
+  },
+  "exampleOrderItem": {
+    "order": {"???": "???"},
+    "product": {},
+    "note": "customization and stuff? this is set by the customer",
+    "quantity": 5,
+    "timePlaced": "???",
+    "timeCompleted": "???",
+    "completedBy": {
+      "id": 2,
+      "email": "employee@email.com",
+      "firstname": "Emplo",
+      "lastname": "Du",
+      "role": "EMPLOYEE",
+      "orders": [],
+      "ratings": [],
+      "stations": []
+    },
+    "status": "FULFILLED"
+  },
+  "exampleRating": {
+    "customer": {},
+    "product": {},
+    "rating": 4
+  }
+};
