7d038a71cfb7155505fc3428f05dff08371de5f7
James Champ <jameschamp@acm.org>
2017-07-02 12:46:10 -0400

    Added getMenu and getBusinesses methods to Service class
    Also, added new method to Product DAO.

--- Orderys/src/main/java/com/revature/orderys/dao/ProductDao.java
+++ Orderys/src/main/java/com/revature/orderys/dao/ProductDao.java
@@ -2,6 +2,7 @@ package com.revature.orderys.dao;
 
 import java.util.List;
 
+import com.revature.orderys.bean.Business;
 import com.revature.orderys.bean.Product;
 
 public interface ProductDao {
@@ -9,6 +10,8 @@ public interface ProductDao {
 
 	List<Product> getAllProducts();
 
+	List<Product> getAllProductsByBusiness(Business business);
+	
 	void createProduct(Product p);
 
 	Product getProductById(long id);
