3365a94147f38c5620ee3c2676cd312c7a73517f
Eric <ericjchristie@gmail.com>
2017-07-02 13:38:25 -0400

    removed an import

--- Orderys/src/main/java/com/revature/orderys/controller/MainController.java
+++ Orderys/src/main/java/com/revature/orderys/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.revature.orderys.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
