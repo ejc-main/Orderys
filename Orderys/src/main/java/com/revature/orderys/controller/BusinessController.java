6629e6c3bbaedbffbb2ae045a47c594068da5a30
Eric <ericjchristie@gmail.com>
2017-07-03 13:34:50 -0400

    fixing merge conflict

--- Orderys/src/main/java/com/revature/orderys/controller/BusinessController.java
+++ Orderys/src/main/java/com/revature/orderys/controller/BusinessController.java
@@ -28,7 +28,6 @@ public class BusinessController {
 		this.service = service;
 	}
 	
-<<<<<<< HEAD
 //	/*
 //	 * GET: get list of all businesses
 //	 */
@@ -127,105 +126,4 @@ public class BusinessController {
 //	 */
 //	@RequestMapping(value="/{businessId}/employee/{employeeId}", method=RequestMethod.DELETE)
 //	
-=======
-	/*
-	 * GET: get list of all businesses
-	 */
-	@RequestMapping(value="", method=RequestMethod.GET)
-	public ArrayList<Business> getAllBusinesses() {
-		return (ArrayList<Business>) service.getAllBusinesses();
-	}
-	
-	/*
-	 * POST: add a new business
-	 */
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public Business addBusiness(@RequestParam(name="businessName", required=true) String businessName,
-			@RequestParam(name="streetAddress1", required=true) String streetAddress1,
-			@RequestParam(name="streetAddress2", required=false) String streetAddress2,
-			@RequestParam(name="city", required=true) String city,
-			@RequestParam(name="state", required=true) String state,
-			@RequestParam(name="country", required=true) String country,
-			@RequestParam(name="zip", required=true) String zip,
-			HttpServletRequest request) {
-		//TODO:implement return
-		return null;
-		
-		
-	}
-	
-	/*
-	 * GET: get information and menu for a specific business
-	 */
-	@RequestMapping(value="/{businessId}", method=RequestMethod.GET)
-	public Business getBusiness(HttpServletRequest request) {
-		//TODO:implement return
-		return null;
-	}
-	
-	/*
-	 * POST: update the information or menu for a specific business
-	 */
-	@RequestMapping(value="/{businessId}", method=RequestMethod.POST)
-	public Business updateBusiness() {
-		//TODO:implement return
-		return null;
-	}
-
-	/*
-	 * GET: get list of stations for a specific business
-	 */
-	RequestMapping(value="/{businessId}/station", method=RequestMethod.GET)
-	
-	/*
-	 * POST: add a new station for a business
-	 */
-	@RequestMapping(value="/{businessId}/station", method=RequestMethod.POST)
-	
-	/*
-	 * GET: get details for a station of a business
-	 */
-	@RequestMapping(value="/{businessId}/station/{stationId}", method=RequestMethod.GET)
-	
-	/*
-	 * POST: update details for a station of a business
-	 */
-	@RequestMapping(value="/{businessId}/station/{stationId}", method=RequestMethod.POST)
-	
-	/*
-	 * DELETE: remove a station
-	 */
-	@RequestMapping(value="/{businessId}/station/{stationId}", method=RequestMethod.DELETE)
-	
-	/*
-	 * GET: get list of products provided by a business (maybe just products associated with the default station)
-	 */
-	@RequestMapping(value="/{businessId}/product", method=RequestMethod.GET)
-	
-	/*
-	 * POST: add a product to the menu (or default station) of a business
-	 */
-	@RequestMapping(value="/{businessId}/product", method=RequestMethod.POST)
-	
-	/*
-	 * GET: get list of all employees associated with a business (or the default station of the business)
-	 */
-	@RequestMapping(value="/{businessId}/employee", method=RequestMethod.GET)
-	
-	/*
-	 * POST: add a user as an employee of a business (possibly assign them to the default station)
-	 */
-	@RequestMapping(value="/{businessId}/employee", method=RequestMethod.POST)
-	
-	/*
-	 * GET: get information about a specific employee
-	 */
-	@RequestMapping(value="/{businessId}/employee/{employeeId}", method=RequestMethod.GET)
-	
-	/*
-	 * DELETE: fire an employee (or remove them from a business for some other reason)
-	 */
-	@RequestMapping(value="/{businessId}/employee/{employeeId}", method=RequestMethod.DELETE)
-	
->>>>>>> f4d5cc5dd11833bb234417ea05d11210cb9433c6
 }
