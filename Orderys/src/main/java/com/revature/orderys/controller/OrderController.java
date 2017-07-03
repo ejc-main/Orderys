c0a767a321e4c7c6604726ad14ee5f90ea7814a3
Eric <ericjchristie@gmail.com>
2017-07-03 13:40:39 -0400

    commenting out unimplemented stuff

--- Orderys/src/main/java/com/revature/orderys/controller/OrderController.java
+++ Orderys/src/main/java/com/revature/orderys/controller/OrderController.java
@@ -10,36 +10,36 @@ import com.revature.orderys.bean.Order;
 @RequestMapping(value="/order")
 public class OrderController {
 	
-	/*
-	 * GET: get the details about a specific order
-	 */
-	@RequestMapping(value="/{orderId}", method=RequestMethod.GET)
-	
-	/*
-	 * POST: update (cancel) a specific order
-	 */
-	@RequestMapping(value="/{orderId}", method=RequestMethod.POST)
-	
-	/*
-	 * GET: get list of order items in an order
-	 */
-	@RequestMapping(value="/{orderId}/item", method=RequestMethod.GET)
-	
-	/*
-	 * [NOT NEEDED?] POST: add an order item to an order
-	 * (would it be best to not allow the items of an order to be modified?
-	 *  - basically orders would be 'assembled' client-side before being submitted)
-	 */
-	@RequestMapping(value="/{orderId}/item", method=RequestMethod.POST)
-
-	/*
-	 * GET: get details for a specific order item in an order
-	 */
-	@RequestMapping(value="/{orderId}/item/{productId}", method=RequestMethod.GET)
-	
-	/*
-	 * POST: update (mark as completed) a specific order item in an order
-	 */
-	@RequestMapping(value="/{orderId}/item/{productId}", method=RequestMethod.POST)
+//	/*
+//	 * GET: get the details about a specific order
+//	 */
+//	@RequestMapping(value="/{orderId}", method=RequestMethod.GET)
+//	
+//	/*
+//	 * POST: update (cancel) a specific order
+//	 */
+//	@RequestMapping(value="/{orderId}", method=RequestMethod.POST)
+//	
+//	/*
+//	 * GET: get list of order items in an order
+//	 */
+//	@RequestMapping(value="/{orderId}/item", method=RequestMethod.GET)
+//	
+//	/*
+//	 * [NOT NEEDED?] POST: add an order item to an order
+//	 * (would it be best to not allow the items of an order to be modified?
+//	 *  - basically orders would be 'assembled' client-side before being submitted)
+//	 */
+//	@RequestMapping(value="/{orderId}/item", method=RequestMethod.POST)
+//
+//	/*
+//	 * GET: get details for a specific order item in an order
+//	 */
+//	@RequestMapping(value="/{orderId}/item/{productId}", method=RequestMethod.GET)
+//	
+//	/*
+//	 * POST: update (mark as completed) a specific order item in an order
+//	 */
+//	@RequestMapping(value="/{orderId}/item/{productId}", method=RequestMethod.POST)
 	
 }
