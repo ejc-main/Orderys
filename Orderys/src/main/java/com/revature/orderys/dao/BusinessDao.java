79f681ec42ce6526b4a41bd23c381ac86708b93f
James Champ <jameschamp@acm.org>
2017-07-03 11:11:43 -0400

    General cleanup
    Removed some unused imports. Removed some TODOs. Added PENDING to
    OrderItem.Status.

--- Orderys/src/main/java/com/revature/orderys/dao/BusinessDao.java
+++ Orderys/src/main/java/com/revature/orderys/dao/BusinessDao.java
@@ -2,14 +2,10 @@ package com.revature.orderys.dao;
 
 import java.util.List;
 
-import org.hibernate.SessionFactory;
-
 import com.revature.orderys.bean.Business;
 import com.revature.orderys.bean.User;
 
 public interface BusinessDao {
-
-
 
 	List<Business> getAllBusinesses();
 
