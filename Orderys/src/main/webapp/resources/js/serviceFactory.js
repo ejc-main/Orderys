f3bca65794cf3ed4bbf8278b099e619aae44e1bb
Eric <ericjchristie@gmail.com>
2017-07-05 05:28:38 -0400

    wrote service factory methods

--- Orderys/src/main/webapp/resources/js/serviceFactory.js
+++ Orderys/src/main/webapp/resources/js/serviceFactory.js
@@ -4,14 +4,56 @@ orderysApp.factory('dataFactory', ['$http', function ($http) {
 	      getUser: function(userId) {
 	        return $http.get('user/' + userId);
 	      },
-	      getAllBusiness: function() {
-		      return $http.get('business/');
+				updateAccount: function(userId, updates) {
+					return $http.post('user/' + userId, updates);
+				},
+				getOrders: function(userId) {
+					return $http.get('user/' + userId + '/order');
+				},
+				placeOrder: function(userId, order) {
+					return $http.post('user/' + userId + '/order', order);
+				},
+				cancelOrder: function(userId, orderId, cancelling) {
+					return $http.post('user/' + userId + '/order/' + orderId, cancelling);
+				},
+	      getAllBusinesses: function() {
+		      return $http.get('business');
 		    },
+				addBusiness: function(business) {
+					return $http.post('business', business);
+				},
 				getBusiness: function(buisnessID) {
 					return $http.get('business/' + buisnessID);
 				},
+				getStations: function(businessId) {
+					return $http.get('business/' + businessId + '/station');
+				},
+				addStation: function(businessId, station) {
+					return $http.post('business/' + businessId + '/station', station);
+				},
+				getActiveOrderItems: function(businessId) {
+					return $http.get('business/' + businessId + '/orderItem');
+				},
 				getProducts: function(buisnessID) {
 					return $http.get('business/' + buisnessID + '/product');
-			 	}
+			 	},
+				addProduct: function(businessId, product) {
+					return $http.post('business/' + businessId + '/product', product);
+				},
+				updateProduct: function(businessId, productId, updates) {
+					return $http.post('business/' + businessId + '/product/' + productId, updates)
+				},
+				getEmployees: function(businessId) {
+					return $http.get('business/' + businessId + '/employee');
+				},
+				addEmployee: function(businessId, emailForm) {
+					return $http.post('business/' + businessId + '/employee', {}, { params: emailForm });
+				},
+				getCompletedOrderItems: function(businessId, employeeId) {
+					return $http.get('business/' + businessId + '/employee/' + employeeId + '/orderItem');
+				},
+				completeOrderItem: function(businessId, employeeId, orderItem) {
+					return $http.post('business/' + businessId + '/employee/' + employeeId + '/orderItem', orderItem);
+				}
 	   };
 	}]); 
